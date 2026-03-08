7aa625944df55807a6e2d44e99b86050cb62c6fb
Tomaz Rocha <53554910+tmzrocha@users.noreply.github.com>
2026-03-07 21:56:20 -0300

    Update Swagger UI title for API
    Alterando titulo de definição em swagger

--- backend/RaphaMovies.API/Program.cs
+++ backend/RaphaMovies.API/Program.cs
@@ -138,7 +138,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rapha Movies API v1");
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "POS TFTEC Movies API v1");
     c.RoutePrefix = "swagger";
 });
 
